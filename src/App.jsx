612f52eee813c2e525df424d23520286c64005c6
aliyedibela <ali.yedibela27@erzurum.edu.tr>
2026-03-17 04:55:10 +0300

    update artik

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useRef, useState, useCallback } from 'react'
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/react"
 import './App.css'
 
 
